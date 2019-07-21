da23692ab4401e3046b63578ef83a82cbcad3083
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-07-21 20:13:20 +0530

    Create check_for_bst.cpp

---
+++ check_for_bst.cpp
@@ -0,0 +1,13 @@
+bool isBST(Node* root) {
+    // Your code here
+   if(!root || (!root->left && !root->right))
+        return true;
+    if(root->left && root->data<root->left->data){
+        return false;
+    }
+    if(root->right && root->data>root->right->data)
+        return false;
+   if(!isBST(root->left)|| !isBST(root->right))
+        return false;
+    return true;
+}
